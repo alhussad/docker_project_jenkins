4a99a561bb8bbe5ca2228e8edf6ccd06b0c25294
alhussad <hussaincstp@gmail.com>
2023-09-02 22:21:13 +0100

    Updated

--- src/test/java/docker/SetupDockerGrid.java
+++ src/test/java/docker/SetupDockerGrid.java
@@ -16,8 +16,8 @@ public class SetupDockerGrid {
     @AfterTest
     public void stopDockerGrid() throws IOException, InterruptedException {
         Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
-//        Thread.sleep(5000);
+        Thread.sleep(5000);
 
-//        Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); //Closes command prompt
+        Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); //Closes command prompt
     }
 }
